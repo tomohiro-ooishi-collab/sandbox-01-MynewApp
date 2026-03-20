745ba2166f4bf88846df3fa1e4cb6e305328f668
Tomohiro Oishi <tomohiro-ooishi@gl.pen-kanagawa.ed.jp>
2026-03-20 14:27:59 +0900

    Initial commit

---
+++ 01-MyfirstAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01-MyfirstAPP
+//
+//  Created by 大石智広 on 2026/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, swift!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
