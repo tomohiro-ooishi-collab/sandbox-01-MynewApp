4bab851ceba7fa9f9f2b3f1b54273eecdb06b1e6
tomohiro-ooishi <tomohiro-ooishi@gl.pen-kanagawa.ed.jp>
2026-04-28 11:58:02 +0900

    Template8

--- 01-MyfirstAPP/ContentView.swift
+++ 01-MyfirstAPP/ContentView.swift
@@ -7,22 +7,15 @@
 
 import SwiftUI
 
-//構造体の定義 型の変更や項目の追加も可能
-struct Item: Identifiable {
-    let id = UUID()
-    let term: String //項目名
-    let meaning: String //意味や説明,詳細
-    var isChecked: Bool   // ← チェックされたか
-}
-
 struct ContentView: View {
-    // データ配列を構造体でまとめる
-    var items = [
-        Item(term: "見本1", meaning: "見本の説明1", isChecked: false),
-        Item(term: "見本2", meaning: "見本の説明2", isChecked: false)
-    ]
     var body: some View {
-        //UIをここに書こう
+        VStack{
+            Image("mainCharacter1")
+                .resizable()
+                .scaledToFit()
+            Text("これは主人公です")
+            
+        }
     }
 }
 
---
+++ 01-MyfirstAPP/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView 2.swift
+//  01-MyfirstAPP
+//
+//  Created by 大石智広 on 2026/04/27.
+//
+
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        VStack{
+            Text("詳細画面")
+        }
+    }
+}
+
